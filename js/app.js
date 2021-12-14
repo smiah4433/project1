83326f8952e40fc33ef9d8346f2f4a433498c79c
smiah4433 <smiah4433@gmail.com>
2021-12-14 12:28:26 -0600

    Arrow keys completed

--- js/app.js
+++ js/app.js
@@ -2,10 +2,21 @@ const canvas = document.getElementById('game')
 const ctx = canvas.getContext('2d')
 
 let speed = 7;
+tileCount = 20;
+let tileSize = canvas.width / tileCount -2
+let headX = 10
+let headY = 10
 
+//moving our snake
+let xVelocity = 0
+let yVelocity = 0
+
+// game loop
 function drawGame() {
   // console.log('draw game');
   clearScreen()
+  changeSnakePosition()
+  drawSnake()
   setTimeout(drawGame, 1000/ speed)
 
 }
@@ -16,6 +27,46 @@ function clearScreen() {
 // console.log(clearScreen);
 }
 console.log(clearScreen);
+
+function drawSnake() {
+  ctx.fillStyle = 'orange'
+  ctx.fillRect(headX * tileCount, headY * tileCount, tileSize,tileSize)
+}
+
+function changeSnakePosition(){
+  headX = headX + xVelocity
+  headY = headY + yVelocity
+
+}
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event) {
+  //up arrow
+  if(event.keyCode == 38){
+    yVelocity = -1;
+    xVelocity = 0;
+  }
+  //down arrow
+  if(event.keyCode == 40){
+    yVelocity = 1;
+    xVelocity = 0;
+  }
+  //right
+  if(event.keyCode == 39){
+    yVelocity = 0;
+    xVelocity = 1;
+  }
+  //left
+  //right
+  if(event.keyCode == 37){
+    yVelocity = 0;
+    xVelocity = -1;
+  }
+}
+
+
+
 
 
 drawGame()
