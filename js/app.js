baf1b87dc2db5fc4d809b3ce04462559e00f512a
smiah4433 <smiah4433@gmail.com>
2021-12-14 12:44:26 -0600

    KeyArrows terminology completed

--- js/app.js
+++ js/app.js
@@ -44,22 +44,30 @@ document.body.addEventListener('keydown', keyDown);
 function keyDown(event) {
   //up arrow
   if(event.keyCode == 38){
+    if (yVelocity == 1) //if you're moving down and try to move up. Nothing will happen
+      return;
     yVelocity = -1;
     xVelocity = 0;
   }
   //down arrow
   if(event.keyCode == 40){
+    if (yVelocity == -1) //if you're moving up and try to move down. Nothing will happen
+      return;
     yVelocity = 1;
     xVelocity = 0;
   }
   //right
   if(event.keyCode == 39){
+    if (xVelocity == -1) //if you're moving left and try to move right. Nothing will happen
+      return;
     yVelocity = 0;
     xVelocity = 1;
   }
   //left
   //right
   if(event.keyCode == 37){
+    if (xVelocity == 1) //if you're moving left and try to move right. Nothing will happen
+      return;
     yVelocity = 0;
     xVelocity = -1;
   }
